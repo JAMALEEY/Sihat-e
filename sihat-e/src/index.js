59413e9fbee5140ccd610675cfef75ae14306764
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-04 12:07:51 +0100

    Applied ReduxForm boilerplate on NewsLetter Form

--- sihat-e/src/index.js
+++ sihat-e/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react';
-import App from './App'
+import ReactDOM from 'react-dom';
+import App from './components/App'
+// in order to work with forms in redux I should work with provider library and wrap up the  App component with it.
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reducers from './reducers';
+import reduxThunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
