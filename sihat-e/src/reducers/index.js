a4dfadab7e97de0372ea5ec35bedb60b677c7801
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-04 10:03:29 +0100

    Creating ReduxForm reducer

---
+++ sihat-e/src/reducers/index.js
@@ -0,0 +1,11 @@
+// I import reducer from the redux-form library so that this reducer manage automatically the states inside the reduxStore
+import { reducer } from "redux-form";
+// To manage my multiple reducers to be one single reducer ill use CombineReducers that takes parameters with what ill call inside my project
+import { combineReducers } from "redux";
+
+
+
+    // I pass in a parameter (form) of a value (reducer) so that I can let redux-form allow the reducer automatization
+export default combineReducers ({
+    form: reducer
+});
