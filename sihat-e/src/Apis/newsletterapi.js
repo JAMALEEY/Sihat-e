44746426130eb3efa56c794fc1aa596498b12fbe
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-04 22:35:51 +0100

    Update newsletterapi.js

--- sihat-e/src/Apis/newsletterapi.js
+++ sihat-e/src/Apis/newsletterapi.js
@@ -1,1 +1,9 @@
 import axios from 'axios'
+
+
+    export default axios.create(
+        {
+            // the API that I created in the backEnd using laravel to use newsletter database
+            baseURL: 'http://127.0.0.1:8000/api/newsletter'
+        }
+    )
