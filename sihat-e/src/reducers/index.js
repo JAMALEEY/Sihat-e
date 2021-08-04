6cf84ac98aefa2444c8f7d41da6963cb7115ef33
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-04 10:09:13 +0100

    create index.js  and append App to it

--- sihat-e/src/reducers/index.js
+++ sihat-e/src/reducers/index.js
@@ -1,5 +1,5 @@
 // I import reducer from the redux-form library so that this reducer manage automatically the states inside the reduxStore
-import { reducer } from "redux-form";
+import { reducer as newsletterReducer } from "redux-form";
 // To manage my multiple reducers to be one single reducer ill use CombineReducers that takes parameters with what ill call inside my project
 import { combineReducers } from "redux";
 
@@ -7,5 +7,5 @@ import { combineReducers } from "redux";
 
     // I pass in a parameter (form) of a value (reducer) so that I can let redux-form allow the reducer automatization
 export default combineReducers ({
-    form: reducer
+    form: newsletterReducer
 });
