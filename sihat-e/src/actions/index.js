d2ff6fcceb004429cf522e96ffa150cdc0491c23
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-04 23:13:32 +0100

    :bug:  Fixed by adding dispatch and replacing component by Api action creator name

--- sihat-e/src/actions/index.js
+++ sihat-e/src/actions/index.js
@@ -1,12 +1,16 @@
 // in order to make requests over our newsletter API we take an instance of this API using axios to apply actions creators on it
 import newsletterapi from "../Apis/newsletterapi";
-import home from "../components/pages/Home";
 
 
 // new action creator to handle newletter api:
 // this is goign to be called with a list of all those different values that we entered into our form as an argument to our action creator (an asynch action creator necessite a reduxThunk)
 export const createNewsletterEmail = (formValues) => {
     return async ( dispatch ) => {
-        home.post('/newsletter', formValues)
-    }   
+        const response = await newsletterapi.post('/newsletter', formValues);
+
+    dispatch ({
+        type: 'CREATE_NEWSLETTER',
+        payload: response.data
+    })
+    };   
 }
