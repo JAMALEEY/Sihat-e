735f1da797ea8c684bdb3b7a6da9e9ca0464930b
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-05 02:08:24 +0100

    Redux User Action Creators

---
+++ sihat-e/src/actions/alert.actions.js
@@ -0,0 +1,21 @@
+    import { alertConstants } from '../constants';
+    export const alertActions = {
+        succes,
+        error,
+        clear
+    };
+
+    // I've wrapped the action methods in an alertActions object to simplifies importing them into other files.
+function success(message) {
+    return { type: alertConstants.SUCCESS, message };
+}
+
+function error(message) {
+    return { type: alertConstants.ERROR, message };
+}
+
+function clear() {
+    return { type: alertConstants.CLEAR };
+}
+
+// The implementation details for each action creator
