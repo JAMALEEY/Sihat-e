8c877bf69483ca5a3a2a06f2c52224505736e3f4
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-05 15:23:17 +0100

    Update user.service.js

--- sihat-e/src/services/user.service.js
+++ sihat-e/src/services/user.service.js
@@ -3,26 +3,12 @@
 // I created the User.service.js file so that I can handle Api in relation with user data, 
 
 // Im gonna use userService object so that I can export all the service methods that contains backend api calls :  
-import config from '../webpack.config';
+import {config} from '../webpack.config';
 // Ill import the authHeader that will give me possibility to work with the Api using unique token (user)
 import { authHeader } from '../helpers/auth-header';
-
-
-
-
-    //  I centralized all the methods that I created below so that I visualize them better and easily exportable 
-    export const userService = {
-    login,
-    logout,
-    register,
-    getAll,
-    getById,
-    update,
-    delete: _delete
-};
-
+ 
     // In the login method the service accepts two arguments username and password it sends request using post and uses headers so it can store iformation about the type of returned data
-
+export const userService = () => {
     function login(username, password) {
     const requestOptions = {
         method: 'POST',
@@ -47,8 +33,9 @@ import { authHeader } from '../helpers/auth-header';
         if (!response.ok) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
-                logout();
-                location.reload(true);
+                return;
+                // logout();
+                // location.reload(true);
             }
 
             const error = (data && data.message) || response.statusText;
@@ -58,3 +45,4 @@ import { authHeader } from '../helpers/auth-header';
         return data;
     });
     }
+}
