396cecff7cef04185ee8336a3aa74d2ed30084d6
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-05 03:54:27 +0100

    Update registration.reducer.js

---
+++ sihat-e/src/reducers/registration.reducer.js
@@ -0,0 +1,19 @@
+// too manage the registration section of the application state
+
+
+import { userConstants } from '../_constants';
+
+    export function registration(state = {}, action) {
+    switch (action.type) {
+        case userConstants.REGISTER_REQUEST:
+            // on registration request it just sets a registering flag set to true 
+        return { registering: true };
+        case userConstants.REGISTER_SUCCESS:
+        return {};
+        case userConstants.REGISTER_FAILURE:
+        return {};
+        // On register success or failure it clears the registration state.
+        default:
+        return state
+    }
+    }
