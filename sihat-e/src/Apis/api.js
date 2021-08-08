f7e5e1f06f828b1af2ef942afea79d3e8707e818
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-08 15:02:03 +0100

    Modified on Api folder helper

--- sihat-e/src/Apis/api.js
+++ sihat-e/src/Apis/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 
-    export default axios.create(
+    const api = axios.create(
         {
             // the API that I created in the backEnd using laravel to use newsletter database
             baseURL: 'http://127.0.0.1:8000/api'
         }
     )
+
+    export default api;
 
