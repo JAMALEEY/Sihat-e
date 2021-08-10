e70ae2ee704a8221d7ef5ead5d18f1ff1ab5b4d9
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-10 23:08:02 +0100

    Update FormDashboardPatient.js

--- sihat-e/src/components/pages/FormDashboardPatient.js
+++ sihat-e/src/components/pages/FormDashboardPatient.js
@@ -1,11 +1,11 @@
-        import React, { Component } from 'react';
-        import { connect } from 'react-redux';
-        import {  logout, createAbout, fetchAboutInfos  } from '../../actions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {  logout, createAbout, fetchAboutInfos  } from '../../actions'
 
 
-    //  const smya = () => {
+//  const smya = () => {
 //             return async () => {
-                
+        
 //                 if(count == 0){
 //                     console.log(this.props.count)
 //                   const save = await this.props.fetchAboutInfos();
@@ -15,9 +15,9 @@
 //                 }else{
 // console.log('hello')
 //                 }
-                
-                
-                
+        
+        
+        
 //             }
 
 //  this.props.setState({
@@ -32,22 +32,22 @@
 //               first_name: this.props.aboutInfos.first_name
 //         })
 //     }
-   
-    
-        class FormDashboardPatient extends Component {
-            constructor(props) {
-                super(props) 
-                
-            if(this.props.aboutInfos.id){
-            localStorage.setItem('myData', JSON.stringify(this.props.aboutInfos));
-            console.log(localStorage.getItem('myData'))
-            } else {
-               
-            }
-            console.log(JSON.parse(localStorage.getItem('myData')))
-            }
-            
+
+
+class FormDashboardPatient extends Component {
+    constructor(props) {
+        super(props) 
         
+    if(this.props.aboutInfos.id){
+    localStorage.setItem('myData', JSON.stringify(this.props.aboutInfos));
+    console.log(localStorage.getItem('myData'))
+    } else {
+       
+    }
+    console.log(JSON.parse(localStorage.getItem('myData')))
+    }
+    
+
 
 // this.state = {
 //         salam : JSON.parse(localStorage.getItem('myData'))
@@ -55,222 +55,175 @@
 //   console.log(this.props.salam)
 
 
-        
 
 
 
 
 
-        
-            state = {
-            first_name: this.props.item.first_name,
-            last_name: this.props.item.last_name,
-            adress: this.props.item.adress,
-            birth_day: this.props.item.birth_day,
-            bio_sex: this.props.item.bio_sex
-            };
-
-            handleChange = this.handleChange.bind(this);
-            handleSubmit = this.handleSubmit.bind(this);
-            
-
-       
-        handleChange(e) {
-            const { target}  = this.event;
-            const value = target.type === 'checkbox' ? target.checked : target.value;
-            const { name }  = target;
-
-            this.setState({
-            [name]: value
-            });
-        }
-
-        handleSubmit(e) {
-            e.preventDefault();
-
-            const { first_name, last_name, adress,birth_day, bio_sex  } = this.state;
-            const body = {
-            first_name, last_name, adress,birth_day, bio_sex
-            };
- localStorage.removeItem('myData');
-        }
 
 
-
-        render() {
-            return (
-                <>
-                <div className="row register-form">
-                    <div className="col-md-8 col-xl-10 offset-md-2 offset-xl-0">
-                    <form className="custom-form" onSubmit={this.handleSubmit}>
-            <h1 className="d-xl-flex align-items-xl-start">A propos</h1>
-
-                <div className="form-row form-group">
-                    <div className="col-sm-4 col-xl-12 label-column">
-                        <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field">
-                            <strong>Nom :</strong>
-                        </label>
-                    </div>
-                    <div className="col-sm-6 col-xl-7 input-column">
-
-                        <input className="form-control" type="text" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).first_name : ''  }
-                        >
-                        </input>
-                    </div> 
-                </div>
-
-
-
-
-
-                <div className="form-row form-group">
-                    <div className="col-sm-4 col-xl-12 label-column">
-                        <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field">
-                            <strong>Prénom :</strong>
-                        </label>
-                    </div>
-
-                    <div className="col-sm-6 col-xl-7 input-column">
-                        <input  className="form-control" type="text" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).last_name : ''  } 
-                        >
-                        </input> 
-                    </div>  
-                </div>
-
-
-                {/* date naissance */}
-                <div className="form-row form-group">
-                    <div className="col-sm-4 col-xl-12 label-column">
-                        <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field"> 
-                        <strong>Date de naissance :</strong></label>
-                    </div>
-
-                    <div className="col-md-6 col-xl-7">
-                        <input className="form-control date" id="birthDate" type="date" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).birth_day : ''  }
-                        >
-                        </input>
-                    </div>
-                </div>
-                {/* end date naissance */}
-
-                {/* adresse */}
-                
-                <div className="form-row form-group">
-                    <div className="col-sm-4 col-xl-12 label-column">
-                        <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field">
-                            <strong>Adresse :</strong>
-                        </label>
-                    </div>
-
-                    <div className="col-sm-6 col-xl-7 input-column">
-                        <input type="text" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).adress : ''  }>
-                        </input> 
-                    </div>  
-                </div>
-                {/* End adresse */}
-
-                {/* Start Radios */}
-                <div>
-                    <fieldset>
-                        <legend />
-                            <div className="custom-control custom-radio">
-                                {JSON.parse(localStorage.getItem('myData')).bio_sex == "femme" ?<input type="radio" id="customRadio1" className="custom-control-input" name="customRadio"  defaultChecked /> : <input type="radio" id="customRadio1" className="custom-control-input" name="customRadio"   /> }
-
-                                <label className="custom-control-label" htmlFor="customRadio1"
-                                >
-                                    Femme
-                                </label>
-                            </div>
-                            
-                            <div className="custom-control custom-radio">
-                                {JSON.parse(localStorage.getItem('myData')).bio_sex == "homme" ?<input type="radio" id="customRadio2" className="custom-control-input" name="customRadio"  defaultChecked  /> : <input type="radio" id="customRadio2" className="custom-control-input" name="customRadio" /> }
-
-                                <label className="custom-control-label" htmlFor="customRadio2">
-                                    Homme
-                                </label>
-                            </div>
-                        </fieldset>
-                </div>
-                {/* End Radios */}
-                    </div>
-                </div>
-                    <button id="btnFormDashboard" className="btn btn-light d-xl-flex align-items-xl-start submit-button" type="submit">Enregistrer
-                    </button>
-                    </form>
-                    </div>
-                </div>
-                {/* End Form */}
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-
-                </div>
-                </>
-
-
-
-
-                
-
-
-
-                {/* <div className="form-row form-group">
-                    <div className="col-sm-4 col-xl-12 label-column">
-                        <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field"> 
-                        <strong>Date de naissance :</strong>
-                        </label>
-                    </div>
-                    <div className="col-md-6 col-xl-7">
-                        <input id="birthDate" type="date" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).birth_day : ''  }
-                        >
-                        </input>
-                    </div>
-                </div> */}
-
-                <div className="form-row form-group">
-                    <div className="col-sm-4 col-xl-7 label-column">
-                        <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="pawssword-input-field">Genre :
-                        </label>
-                    </div>
-                    <div className="col-sm-6 input-column">
-                            {/* Start: Bootstrap 4's Custom Radios & Checkboxes */}
-                            <div>
-                            <fieldset>
-                                <legend />
-                    <div className="custom-control custom-radio"> 
-
-                     {JSON.parse(localStorage.getItem('myData')).bio_sex == "femme" ?<input type="radio" id="customRadio1" className="custom-control-input" name="customRadio"  defaultChecked /> : <input type="radio" id="customRadio1" className="custom-control-input" name="customRadio"   /> }
-                   
-                    <label className="custom-control-label" htmlFor="customRadio1">Femme</label></div>
-
-                <div className="custom-control custom-radio">
-
-                     {JSON.parse(localStorage.getItem('myData')).bio_sex == "homme" ?<input type="radio" id="customRadio2" className="custom-control-input" name="customRadio"  defaultChecked  /> : <input type="radio" id="customRadio2" className="custom-control-input" name="customRadio" /> }
-                   </div>
-                <label className="custom-control-label" htmlFor="customRadio2">Homme</label></div>
-                </fieldset>
-                            </div>{/* End: Bootstrap 4's Custom Radios & Checkboxes */}
-                </div>
-                
-                </div>
-                <button  id="btnFormDashboard" className="btn btn-light d-xl-flex align-items-xl-start submit-button" type="submit" >Update</button>
-            </form>
-            </>
-            );
-        }
-        }
-    const mapStateToProps = state => {
-      return {
-        aboutInfos: state.aboutInfos,
-        logout: state.logout
-      };
+    state = {
+    first_name: this.props.item.first_name,
+    last_name: this.props.item.last_name,
+    adress: this.props.item.adress,
+    birth_day: this.props.item.birth_day,
+    bio_sex: this.props.item.bio_sex
     };
 
+    handleChange = this.handleChange.bind(this);
+    handleSubmit = this.handleSubmit.bind(this);
+    
 
 
-    export default connect(mapStateToProps, {fetchAboutInfos})(FormDashboardPatient);
+handleChange(e) {
+    const { target}  = this.event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name }  = target;
+
+    this.setState({
+    [name]: value
+    });
+}
+
+handleSubmit(e) {
+    e.preventDefault();
+
+    const { first_name, last_name, adress,birth_day, bio_sex  } = this.state;
+    const body = {
+    first_name, last_name, adress,birth_day, bio_sex
+    };
+localStorage.removeItem('myData');
+}
+
+
+
+render() {
+    return (
+        <>
+    <div className="d-xl-flex justify-content-xl-center align-items-xl-center">
+    <div id="formCardContainer">
+        <div>
+                        {/* Start Form */}
+        <div className="row register-form">
+            <div className="col-md-8 col-xl-10 offset-md-2 offset-xl-0">
+            <form className="custom-form" onSubmit={this.handleSubmit}>
+    <h1 className="d-xl-flex align-items-xl-start">A propos</h1>
+
+        <div className="form-row form-group">
+            <div className="col-sm-4 col-xl-12 label-column">
+                <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field">
+                    <strong>Nom :</strong>
+                </label>
+            </div>
+            <div className="col-sm-6 col-xl-7 input-column">
+
+                <input className="form-control" type="text" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).first_name : ''  }
+                />
+            </div> 
+        </div>
+
+
+
+
+
+        <div className="form-row form-group">
+            <div className="col-sm-4 col-xl-12 label-column">
+                <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field">
+                    <strong>Prénom :</strong>
+                </label>
+            </div>
+
+            <div className="col-sm-6 col-xl-7 input-column">
+                <input  className="form-control" type="text" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).last_name : ''  } 
+                >
+                </input> 
+            </div>  
+        </div>
+
+
+        {/* date naissance */}
+        <div className="form-row form-group">
+            <div className="col-sm-4 col-xl-12 label-column">
+                <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field"> 
+                <strong>Date de naissance :</strong></label>
+            </div>
+
+            <div className="col-md-6 col-xl-7">
+                <input className="form-control date" id="birthDate" type="date" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).birth_day : ''  }
+                >
+                </input>
+            </div>
+        </div>
+        {/* end date naissance */}
+
+        {/* adresse */}
+        
+        <div className="form-row form-group">
+            <div className="col-sm-4 col-xl-12 label-column">
+                <label className="col-form-label d-xl-flex align-items-xl-start" htmlFor="name-input-field">
+                    <strong>Adresse :</strong>
+                </label>
+            </div>
+
+            <div className="col-sm-6 col-xl-7 input-column">
+                <input className="form-control" type="text" defaultValue={ localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')).adress : ''  }>
+                </input> 
+            </div>  
+        </div>
+        {/* End adresse */}
+
+        {/* Start Radios */}
+        <div>
+            
+                    <div className="custom-control custom-radio">
+                        {JSON.parse(localStorage.getItem('myData')).bio_sex == "femme" ?<input type="radio" id="customRadio1" className="custom-control-input" name="customRadio"  defaultChecked /> : <input type="radio" id="customRadio1" className="custom-control-input" name="customRadio"   /> }
+
+                        <label className="custom-control-label" htmlFor="customRadio1"
+                        >
+                            Femme
+                        </label>
+                    </div>
+                    
+                    <div className="custom-control custom-radio">
+                        {JSON.parse(localStorage.getItem('myData')).bio_sex == "homme" ?<input type="radio" id="customRadio2" className="custom-control-input" name="customRadio"  defaultChecked  /> : <input type="radio" id="customRadio2" className="custom-control-input" name="customRadio" /> }
+
+                        <label className="custom-control-label" htmlFor="customRadio2">
+                            Homme
+                        </label>
+                    </div>
+                
+        </div>
+        {/* End Radios */}
+            
+
+        
+            <button id="btnFormDashboard" className="btn btn-light d-xl-flex align-items-xl-start submit-button" type="submit">Enregistrer
+            </button>
+            </form>
+            </div>
+        </div>
+        {/* End Form */}
+        </div>
+    </div>
+    </div>
+
+
+
+        
+        </>
+    );
+}
+}
+const mapStateToProps = state => {
+return {
+aboutInfos: state.aboutInfos,
+logout: state.logout
+};
+};
+
+
+
+export default connect(mapStateToProps, {fetchAboutInfos})(FormDashboardPatient);
 
 
 
