4afef30c27a69367ddac5c217642fbecd412ca76
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-08-10 17:13:03 +0100

    Render a new form

--- sihat-e/src/components/pages/DashboardPatient.js
+++ sihat-e/src/components/pages/DashboardPatient.js
@@ -1,60 +1,131 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import {  logout, createAbout, fetchAboutInfos  } from '../../actions'
-import {Field, formValueSelector, reduxForm, touch} from 'redux-form';
+import {Field, Form, formValueSelector, reduxForm, touch} from 'redux-form';
+import FormDashboardPatient from "./FormDashboardPatient";
+
+    class DashboardPatient extends Component {
+        componentDidMount(){
+
+        }
+        state = {
+    name: 'React',
+    infos: [
+    { uId: 1, first_name: 'JAMAL EDD1441INE', last_name: 'YASSIR', adress: 'CASA', birth_day: '15/11/1997', bio_sex: ''},
+    
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.infos.map(info => <FormDashboardPatient key={info.uId} item={info} data={this.props.location.data} />)}
+      </div>
+    );
+  }
+}
+
+    export default DashboardPatient;
 
 
 
 
-        const errorsHelper = ({error, touched}) => {
-            if (touched && error) {
-                return (
-                    <>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // const errorsHelper = ({error, touched}) => {
+        //     if (touched && error) {
+        //         return (
+        //             <>
                     
-                        <div className='taken'>
-                            <div>
-                                <strong> &#9888; Attention &#9888; </strong> 
-                                {error} 
-                            </div>
-                        </div>
-                    </>
-                )
-            }
-        }
+        //                 <div className='taken'>
+        //                     <div>
+        //                         <strong> &#9888; Attention &#9888; </strong> 
+        //                         {error} 
+        //                     </div>
+        //                 </div>
+        //             </>
+        //         )
+        //     }
+        // }
 
-        const renderInput = (props) => {
-            const {input, value, meta, label, placeholder, name, id, type, className, initialValues} = props;
-            return (
-                <>
-                    {   errorsHelper(meta)    }
-                    <div className="col-sm-6 col-xl-7 input-column">
-                        <div className="form-row form-group">
-                        <label className="active col-form-label d-xl-flex align-items-xl-start">{label}</label>
-                        <input {...input}
-                        className={className}
-                        autocomplete='nope' 
-                        placeholder={placeholder} 
-                        onChange={input.onChange} 
-                        // value={input.value} 
+        // const renderInput = (props) => {
+        //     const {input, value, meta, label, placeholder, name, id, type, className, initialValues} = props;
+        //     return (
+        //         <>
+        //             {   errorsHelper(meta)    }
+        //             <div className="col-sm-6 col-xl-7 input-column">
+        //                 <div className="form-row form-group">
+        //                 <label className="active col-form-label d-xl-flex align-items-xl-start">{label}</label>
+        //                 <input {...input}
+        //                 className={className}
+        //                 autocomplete='nope' 
+        //                 placeholder={placeholder} 
+        //                 onChange={input.onChange} 
+        //                 // value={input.value} 
                         
-                        name={name}
-                        type={type}
-                        id={id}
-                        />
-                        </div>
-                    </div>  
-                </>
-            )
-        }
+        //                 name={name}
+        //                 type={type}
+        //                 id={id}
+        //                 />
+        //                 </div>
+        //             </div>  
+        //         </>
+        //     )
+        // }
 
-    const DashboardPatient = ( props ) => {
-        const {aboutInfos, logout, handleSubmit, createAbout, fetchAboutInfos,  initialValues} = props
-            useEffect(() => {
-                fetchAboutInfos()
+    // const DashboardPatient = ( props ) => {
+    //     const {aboutInfos, logout, handleSubmit, createAbout, fetchAboutInfos,  initialValues} = props
+            // useEffect(() => {
+            //     fetchAboutInfos()
                 
                 // console.log(aboutInfos[0].data) throw : {id: 7, user_id: 6, first_name: "AVC", last_name: "AAA", birth_day: null, …}
-            }, [])
+            // }, [])
     //             const data = {
     //     initialValues
     // }
@@ -82,49 +153,49 @@ import {Field, formValueSelector, reduxForm, touch} from 'redux-form';
             // if(fetchAboutInfos.PatientAboutInfosNotInfos)
             
             
-            const renderList = () => {
-                if(fetchAboutInfos() == true ) {
-                    return (
-                        <>
-                        <div className="item" key={props.aboutInfos[0].data.id}>
-                            <div className='content'> {props.aboutInfos[0].data.first_name} </div>
-                        </div>
+            // const renderList = () => {
+            //     if(fetchAboutInfos() == true ) {
+            //         return (
+            //             <>
+            //             <div className="item" key={props.aboutInfos[0].data.id}>
+            //                 <div className='content'> {props.aboutInfos[0].data.first_name} </div>
+            //             </div>
 
-                        </>
-                    )
-                }
+            //             </>
+            //         )
+            //     }
                     // console.log(aboutInfos[0].data)
 
-            } 
+        //     } 
             
-            const patientDashboarLogout = () => {
-                logout()
-            }
+        //     const patientDashboarLogout = () => {
+        //         logout()
+        //     }
 
-        const onSubmit = (formValues) => {
+        // const onSubmit = (formValues) => {
             // if( !aboutInfos[0]) {
             //     createAbout(formValues) 
             // } else {
             //     fetchAboutInfos()
             // }
             
-        }
+        // }
 
-        return(
+        // return(
 
-                <>
+        //         <>
                
-                </>
-            )
+        //         </>
+        //     )
 
 
-    }
+    // }
 
     // mapStateToProps = (state) => {
 
     // }
 
 
-const  formWrapper = reduxForm({ form: 'dashboardPatientForm', enableReinitialize: true })(DashboardPatient)
+// const  formWrapper = reduxForm({ form: 'dashboardPatientForm', enableReinitialize: true })(DashboardPatient)
 
-export default connect(null, {logout, createAbout, fetchAboutInfos, })(formWrapper);
+// export default connect(null, {logout, createAbout, fetchAboutInfos, })(formWrapper);
